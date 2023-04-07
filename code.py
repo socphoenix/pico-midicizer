58fbc3e258b8158771da9caedbba1880793407b2
socphoenix <tuc03516@gmail.com>
2023-04-07 08:12:38 -0600

    add changes to how set button is processed
    also removes gc.collect as it is unneeded.

--- code.py
+++ code.py
@@ -1,23 +1,3 @@
-#Copyright (c) 2023 Michael Bell
-#
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-#
-#The above copyright notice and this permission notice shall be included in
-#all copies or substantial portions of the Software.
-#
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-#THE SOFTWARE.
-
 import board
 import digitalio
 from digitalio import DigitalInOut, Direction, Pull
@@ -224,8 +204,6 @@ for x in range(183, 215):
 
 #main program loop
 while True:
-	#garbage collecting
-	gc.collect()
 	#reset noteNum at start of loop
 	noteNum = 1
 	midiNum = 36
