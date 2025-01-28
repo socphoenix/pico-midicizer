51b7d9671cee0132b57573da3df606f3bb23320f
Michael Bell <tuc03516@gmail.com>
2025-01-27 20:42:51 -0700

    update code.py with toe-stud code, and comments on where to cut if not using

--- code.py
+++ code.py
@@ -27,7 +27,6 @@ import adafruit_midi
 from adafruit_midi.control_change import ControlChange
 from adafruit_midi.note_on import NoteOn
 from adafruit_midi.note_off import NoteOff
-import gc
 import busio
 from analogio import AnalogIn
 
@@ -36,6 +35,7 @@ swellShoe = AnalogIn(board.GP26)
 swellValue = swellShoe.value // 512
 choirShoe = AnalogIn(board.GP27)
 choirValue = choirShoe.value // 512
+cnclLast = 1
 
 usb_midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1], out_channel=10)
 #set up lists
@@ -65,20 +65,8 @@ st = DigitalInOut(board.GP5)
 st.direction = Direction.INPUT
 st.pull = Pull.UP
 
-#mem-
-memMinus = DigitalInOut(board.GP6)
-memMinus.direction = Direction.INPUT
-memMinus.pull = Pull.UP
-
-#mem+
-memPlus = DigitalInOut(board.GP7)
-memPlus.direction = Direction.INPUT
-memPlus.pull = Pull.UP
-
-
-
 #for loop for presets 0-11:
-pinsPresets = [board.GP8,board.GP9,board.GP10,board.GP11,board.GP12,board.GP13,board.GP14,board.GP18,board.GP19,board.GP20,board.GP21,board.GP22]
+pinsPresets = [board.GP12,board.GP8,board.GP9,board.GP14,board.GP10,board.GP11,board.GP13,board.GP19,board.GP20,board.GP21,board.GP15,board.GP22]
 
 for x in range(12):
 	presets[x] = DigitalInOut(pinsPresets[x])
@@ -210,6 +198,16 @@ for b in range(16):
 	mcpAddresses[z].direction = digitalio.Direction.INPUT
 	mcpAddresses[z].pull = digitalio.Pull.UP
 	z = z + 1
+a = a + 1
+mcp = MCP23017(i2c1, address = mcpCodes[a])
+
+#this is for the toe studs, remove 205-209, and the ending from the toe-stud removal comment onwards if not using.
+for b in range(12):
+	mcpAddresses.append(mcp.get_pin(b))
+	mcpAddresses[z].direction = digitalio.Direction.INPUT
+	mcpAddresses[z].pull = digitalio.Pull.UP
+	z = z + 1
+
 
 #free memory deleting unneeded variable for hex addresses
 del mcpCodes
@@ -232,85 +230,127 @@ while True:
 	usb_midi.out_channel = USB_MIDI_channel - 1
 
 	#expression pedals
-	if swellShoe.value // 512 != swellValue and swellShoe.value >= swellValue+3 or swellShoe.value <= swellValue-3:
-		swellValue = swellShoe.value // 512
+	try:
+		currentSwell = swellShoe.value
+	except:
+		error = 1
+	if currentSwell // 512 != swellValue and currentSwell >= swellValue+3 or currentSwell <= swellValue-3:
+		swellValue = currentSwell // 512
 		usb_midi.send(ControlChange(1, swellValue))
-	if choirShoe.value // 512 != choirValue and choirShoe.value >=swellValue+3 or swellShoe.value <= swellValue-3:
-		choirValue = choirShoe.value // 512
+	try:
+		currentChoir = choirShoe.value
+	except:
+		error = 1
+	if currentChoir // 512 != choirValue and currentChoir >=choirValue+3 or currentChoir <= choirValue-3:
+		choirValue = currentChoir // 512
 		usb_midi.send(ControlChange(2, choirValue))
 
 #cancel then set then mem minus then memplus buttons.
 
-	if cncl.value == 0:
+	try:
+		curCncl = cncl.value
+	except:
+		error = 1
+	if curCncl == 0:
 		usb_midi.send(NoteOn(60, 64))
-	if st.value == 0 and st.value != setStatus:
+		cnclLast = 0
+	if curCncl == 1 and cnclLast == 0:
+		usb_midi.send(NoteOff(60, 64))
+		cnclLast = 1
+	try:
+		curSt = st.value
+	except:
+		error = 1
+	if curSt == 0 and curSt != setStatus:
 		usb_midi.send(NoteOn(61, 64))
 		setStatus = 0
-	if st.value == 1 and st.value != setStatus:
+	if curSt == 1 and curSt != setStatus:
 		usb_midi.send(NoteOff(61, 64))
 		setStatus = 1
-	if memMinus.value == 0:
-		usb_midi.send(NoteOn(62, 64))
-	if memPlus.value == 0:
-		usb_midi.send(NoteOn(63, 64))
 	#check presets and increment z to send unique midi signal for each preset button
 	z = 64
-	for x in range(len(presets)):
-		if presets[x].value == 0:
-			usb_midi.send(NoteOn(z, 64))
-		z = z + 1
+	try:
+		for x in range(len(presets)):
+			if presets[x].value == 0:
+				usb_midi.send(NoteOn(z, 64))
+			z = z + 1
+	except:
+		pass
 
 #begin looping through keyboards going swell great choir pedals:
 	USB_MIDI_channel = 1
 	midiNum = 36
 	usb_midi.out_channel = USB_MIDI_channel - 1
-	for x in range(61):
-		y = mcpAddresses[x].value
-		if y == 0 and y != noteStatus[x]:
-			noteStatus[x] = y
-			usb_midi.send(NoteOn(midiNum, 64))
-		elif y == 1 and y != noteStatus[x]:
-			noteStatus[x] = y
-			usb_midi.send(NoteOff(midiNum, 64))
-		midiNum = midiNum + 1
+	try:
+		for x in range(61):
+			y = mcpAddresses[x].value
+			if y == 0 and y != noteStatus[x]:
+				noteStatus[x] = y
+				usb_midi.send(NoteOn(midiNum, 64))
+			elif y == 1 and y != noteStatus[x]:
+				noteStatus[x] = y
+				usb_midi.send(NoteOff(midiNum, 64))
+			midiNum = midiNum + 1
+	except:
+		error = 1
 	#begin great
 	USB_MIDI_channel = 2
 	midiNum = 36
 	usb_midi.out_channel = USB_MIDI_channel - 1
-	for x in range(61, 122):
-		y = mcpAddresses[x].value
-		if y == 0 and y != noteStatus[x]:
-			noteStatus[x] = y
-			usb_midi.send(NoteOn(midiNum, 64))
-		elif y == 1 and y != noteStatus[x]:
-			noteStatus[x] = y
-			usb_midi.send(NoteOff(midiNum, 64))
-		midiNum = midiNum + 1
+	try:
+		for x in range(61, 122):
+			y = mcpAddresses[x].value
+			if y == 0 and y != noteStatus[x]:
+				noteStatus[x] = y
+				usb_midi.send(NoteOn(midiNum, 64))
+			elif y == 1 and y != noteStatus[x]:
+				noteStatus[x] = y
+				usb_midi.send(NoteOff(midiNum, 64))
+			midiNum = midiNum + 1
+	except:
+		error = 1
 	#begin choir
 	USB_MIDI_channel = 3
 	midiNum = 36
 	usb_midi.out_channel = USB_MIDI_channel - 1
-	for x in range(122, 183):
-		y = mcpAddresses[x].value
-		if y == 0 and y != noteStatus[x]:
-			noteStatus[x] = y
-			usb_midi.send(NoteOn(midiNum, 64))
-		elif y == 1 and y != noteStatus[x]:
-			noteStatus[x] = y
-			usb_midi.send(NoteOff(midiNum, 64))
-		midiNum = midiNum + 1
+	try:
+		for x in range(122, 183):
+			y = mcpAddresses[x].value
+			if y == 0 and y != noteStatus[x]:
+				noteStatus[x] = y
+				usb_midi.send(NoteOn(midiNum, 64))
+			elif y == 1 and y != noteStatus[x]:
+				noteStatus[x] = y
+				usb_midi.send(NoteOff(midiNum, 64))
+			midiNum = midiNum + 1
+	except:
+		error = 1
 	#begin pedals
 	USB_MIDI_channel = 4
 	midiNum = 36
 	usb_midi.out_channel = USB_MIDI_channel - 1
-	for x in range(183, 215):
-		y = mcpAddresses[x].value
-		if y == 1 and y != noteStatus[x]:
-			noteStatus[x] = y
-			usb_midi.send(NoteOn(midiNum, 64))
-		elif y == 0 and y != noteStatus[x]:
-			noteStatus[x] = y
-			usb_midi.send(NoteOff(midiNum, 64))
-		midiNum = midiNum + 1
+	try:
+		for x in range(183, 215):
+			y = mcpAddresses[x].value
+			if y == 1 and y != noteStatus[x]:
+				noteStatus[x] = y
+				usb_midi.send(NoteOn(midiNum, 64))
+			elif y == 0 and y != noteStatus[x]:
+				noteStatus[x] = y
+				usb_midi.send(NoteOff(midiNum, 64))
+			midiNum = midiNum + 1
+	except:
+		error = 1
 
-
+    #begin footswitches: cut from here to end if you don't want foot switches, as well as lines 205-209 (it is marked with a comment)
+	USB_MIDI_channel = 16
+	usb_midi.out_channel = USB_MIDI_channel - 1
+    #check presets and increment z to send unique midi signal for each preset button
+	z = 64
+	try:
+		for x in range(215, 227)):
+			if mcpAddresses[x].value == 0:
+				usb_midi.send(NoteOn(z, 64))
+			z = z + 1
+	except:
+		error = 1
